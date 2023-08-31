c5055482dd735abece061ac57c7f44adaf94270f
parikshit7757 <parikshitpatil300@gmail.com>
2023-08-31 19:31:41 +0530

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM openjdk:17 version
+
+MAINTAINER Ashok<ashokitschool@gmail.com>
+
+COPY target/app.jar /usr/app/app.jar 
+
+EXPOSE 8080
+
+int i = 10
+
+
---
+++ src/main/java/in/ashokit/App.java
@@ -0,0 +1,20 @@
+package in.ashokit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Ashok IT..." );
+		
+		// Task - 101 changes
+		int i  = 10;
+		int j = 20;
+		int k = i+j;
+		
+		System.out.println(k);
+    }
+}
